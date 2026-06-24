85566f796fdb38bfd7c0c245a06a9b7dc541197b
Vanessza <schabvanessza@gmail.com>
2026-06-24 12:42:11 +0200

    Create lib.rs

---
+++ lib.rs
@@ -0,0 +1,19 @@
+#![no_std]
+
+use embassy_time::Timer;
+
+
+pub async fn beep(beeper: &mut esp_hal::gpio::Output<'_>, status: bool, beep_number: u32){
+    if status == false{
+    }
+    else{
+        for _ in 0..beep_number{
+            beeper.set_high();
+            Timer::after_millis(1000).await;
+            beeper.set_low();
+            Timer::after_millis(1000).await;
+        }
+    }
+}
+
+
